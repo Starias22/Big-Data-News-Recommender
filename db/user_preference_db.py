d44d4a33365d96fc66b2b21ce90b33425337a87a
Starias22 <adedeezechiel@gmail.com>
2024-06-14 23:24:03 +0100

    User preference db class added

---
+++ db/user_preference_db.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+from src.profiles.user_preference import UserPreference
+
+class UserPreferenceDB:
+    def __init__(self, uri, db_name):
+        self.client = MongoClient(uri)
+        self.db = self.client[db_name]
+
+    def save_user_preference(self, user_preference):
+        user_preference_dict = user_preference.to_dict()
+        result = self.db.user_preferences.insert_one(user_preference_dict)
+        return result.inserted_id
+
+    def find_user_preference_by_id(self, user_id):
+        user_preference_data = self.db.user_preferences.find_one({"id": user_id})
+        if user_preference_data:
+            return UserPreference.from_dict(user_preference_data)
+        return None
