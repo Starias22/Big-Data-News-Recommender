0dfdf90fc5c6b61c3cc78ebaa67c6eaeab459a69
Starias22 <adedeezechiel@gmail.com>
2024-06-16 16:19:22 +0100

    User, filtered news and interaction classes created

--- src/profiles/user.py
+++ src/profiles/user.py
@@ -1,23 +1,25 @@
 class User:
-    def __init__(self, id=None,firstname=None, lastname=None, email=None,
-                 categories=[],
-                 sentiments=[],seen_news=[]):
-        self.id=id
-        self.firstname=firstname
-        self.lastname=lastname
-        self.email=email
-        self.categories=[]
-        self.sentiments=[]
-        self.seen_news=[]
-
+    def __init__(self, id=None, firstname=None, lastname=None, email=None,
+                 categories=[i for i in range(32)], sentiments=[0,-1,1], seen_news=[],
+                 ansa=True):
+        self.id = id
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.categories = categories
+        self.sentiments = sentiments
+        self.seen_news = seen_news 
+        self.ansa=ansa
 
     def to_dict(self):
         return {
-            #"id": self.id,
             "firstname": self.firstname,
             "lastname": self.lastname,
             "email": self.email,
-            #"preferences":
+            "categories": self.categories,
+            "sentiments": self.sentiments,
+            "seen_news": self.seen_news,
+            "ansa":self.ansa
         }
 
     @staticmethod
@@ -26,10 +28,26 @@ class User:
             id=data.get('_id'),
             firstname=data['firstname'],
             lastname=data['lastname'],
-            email=data['email']
+            email=data['email'],
+            categories=data.get('categories'),
+            sentiments=data.get('sentiments'),
+            seen_news=data.get('seen_news'),
+            ansa=data.get('ansa')
+        )
+
+    @staticmethod
+    def retrieve_preferences(data):
+        return User(
+            id=data.get('_id'),
+            categories=data.get('categories'),
+            sentiments=data.get('sentiments'),
+            seen_news=data.get('seen_news'),
+            ansa=data.get('ansa'),
+            email=data.get('email'),
+
         )
 
     def display(self):
-        print('Firstname:',self.firstname)
-        print('Lastname:',self.lastname)
-        print('Email:',self.email)
+        print('Firstname:', self.firstname)
+        print('Lastname:', self.lastname)
+        print('Email:', self.email)
