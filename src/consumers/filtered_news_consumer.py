130ee5e3877f626869a7bf333017908893336f48
Starias22 <adedeezechiel@gmail.com>
2024-06-17 01:33:23 +0100

    Filtered news consumer is up to date

--- src/consumers/filtered_news_consumer.py
+++ src/consumers/filtered_news_consumer.py
@@ -7,41 +7,51 @@ import sys
 src_path = Path(__file__).resolve().parents[1]
 sys.path.append(str(src_path))
 
-from profiles.filtered_news import FilteredNews  # Make sure to import the FilteredNews class from the appropriate module
-from db.filtered_news_db import FilteredNewsDB  # Ensure the correct path to FilteredNewsDB
+from profiles.filtered_news import FilteredNews
+from db.filtered_news_db import FilteredNewsDB
 
-# Load the configuration from the JSON file
-with open('../../config/config.json', 'r') as config_file:
-    config = json.load(config_file)
 
-# List of topics to subscribe to
-topics = [config["filtered_news_topic"]]
 
-# Initialize the consumer
-consumer = KafkaConsumer(
-    *topics,  # Unpack the list of topics
-    bootstrap_servers=config["kafka_bootstrap_servers"],
-    auto_offset_reset='earliest',  # Start reading from the earliest message
-    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
-)
+def persist_filtered_news(topics=None ,
+       servers=None):
+     
+    if topics is None and servers  is None:
+         # Load the configuration from the JSON file
+        with open('../../config/config.json', 'r') as config_file:
+            config = json.load(config_file)
 
-print("Kafka Consumer Initialized")
+            # List of topics to subscribe to
+            topics = [config["filtered_news_topic"]]
+            servers=config["kafka_bootstrap_servers"]
+            
+    # Initialize the consumer
+    consumer = KafkaConsumer(
+        *topics,  # Unpack the list of topics
+        bootstrap_servers=servers,
+        auto_offset_reset='earliest',  # Start reading from the earliest message
+        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    )
 
-# Initialize MongoDB filtered news database
-filtered_news_db = FilteredNewsDB()
+    print("Kafka Consumer Initialized")
 
-print(f"Connected to MongoDB")
+    # Initialize MongoDB filtered news database
+    filtered_news_db = FilteredNewsDB()
 
-# Consume messages from the subscribed topics
-for n, message in enumerate(consumer):
-    filtered_news_data = message.value
-    print(f"Received message {n + 1}: {filtered_news_data}")
+    print(f"Connected to MongoDB")
 
-    # Deserialize the message into a FilteredNews object
-    filtered_news = FilteredNews.from_dict(filtered_news_data)
+    # Consume messages from the subscribed topics
+    for n, message in enumerate(consumer):
+        filtered_news_data = message.value
+        print(f"Received message {n + 1}: {filtered_news_data}")
 
-    # Insert filtered news into MongoDB
-    result = filtered_news_db.create_filtered_news(filtered_news)
-    print(f"Inserted filtered news with ID: {result}")
+        # Deserialize the message into a FilteredNews object
+        filtered_news = FilteredNews.from_dict(filtered_news_data)
 
-print(f"Processed {n + 1} messages")
+        # Insert filtered news into MongoDB
+        result = filtered_news_db.create_filtered_news(filtered_news)
+        print(f"Inserted filtered news with ID: {result}")
+
+    print(f"Processed {n + 1} messages")
+
+if __name__=="__main__":
+    persist_filtered_news()
