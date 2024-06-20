63d5187d9d2191e2fb88da9b295b369e9427da69
Starias22 <adedeezechiel@gmail.com>
2024-06-20 12:09:10 +0100

    News source and publication duration formatting handled

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import sys
 import os
+import datetime
+
 
 
 # Add the project root directory to the sys.path
@@ -50,7 +52,48 @@ def send_email(receiver_addr,subject,body):
 
     return sent
 
+def format_source(name,author):
+    if (name is None and author is None) or name is None:
+        return "Source Unkown"
+    
+    if author is None:
+        return name
+    return f"{name} | {author}"
+def format_duration(timestamp):
+    if timestamp is None:
+        return "(Time unknown)"    
+    # Convert the timestamp to a datetime object
+    dt_object = datetime.datetime.fromtimestamp(timestamp)
+
+    # Get the current time
+    current_time = datetime.datetime.now()
+
+    # Calculate the duration
+    duration = current_time - dt_object
+
+    # Get the duration in days, hours, minutes, and seconds
+    days = duration.total_seconds() // 86400
+    hours = (duration.total_seconds() % 86400) // 3600
+    minutes = (duration.total_seconds() % 3600) // 60
+    seconds = duration.total_seconds() % 60
+
+    # Format the duration based on the condition
+    if days >= 1:
+        formatted_duration = f"{int(days)}d"
+    elif hours >= 1:
+        formatted_duration = f"{int(hours)}h"
+    elif minutes >= 1:
+        formatted_duration = f"{int(minutes)}m"
+    else:
+        formatted_duration = f"{int(seconds)}s"
+
+    # Print the formatted date and time
+    return formatted_duration
+
 #SMTPRecipientsRefused(senderrs)
 if __name__=="__main__":
-    x=send_email(receiver_addr='ezechieladede@gmail.com',subject='Test',body='This is just for test')
-    print(x)
+    pass
+    #x=send_email(receiver_addr='ezechieladede@gmail.com',subject='Test',body='This is just for test')
+    #print(x)
+    #timestamp =1718713800
+    #print(format_duration(timestamp))
