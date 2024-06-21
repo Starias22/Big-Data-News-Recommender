9a5415919112ef3a0a6520133030ac52eb1d5172
Starias22 <adedeezechiel@gmail.com>
2024-06-21 19:03:50 +0100

    Features indexing issue resolved

--- src/consumers/similarity.py
+++ src/consumers/similarity.py
@@ -4,17 +4,17 @@ from scipy.spatial.distance import cosine
 
 # Function to compute similarity between two sparse vectors
 def compute_sparse_vector_similarity(features1, features2):
-    #print('features1=',features1)
-    #print('features2=',features2)
+    print('features1=',features1)
+    print('features2=',features2)
     #features2=features2['features']
-
-    size = max(int(features1['size']), int(features2['size']))
+   
+    size = max(int(features1['size']), int(features2[0]))
 
     # Convert to sparse matrix format
     vec1 = csr_matrix((np.array(features1['values'], dtype=float),
                        (np.zeros(len(features1['indices'])), np.array(features1['indices'], dtype=int))), shape=(1, size))
-    vec2 = csr_matrix((np.array(features2['values'], dtype=float),
-                       (np.zeros(len(features2['indices'])), np.array(features2['indices'], dtype=int))), shape=(1, size))
+    vec2 = csr_matrix((np.array(features2[2], dtype=float),
+                       (np.zeros(len(features2[1])), np.array(features2[1], dtype=int))), shape=(1, size))
 
     # Compute cosine similarity
     similarity = 1 - cosine(vec1.toarray().flatten(), vec2.toarray().flatten())
